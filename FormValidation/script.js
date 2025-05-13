21d46c3d4df2e072ac0f0975e8a88c3f7273cb20
Faramarz Javdan <javdanfar@outlook.com>
2025-05-13 23:28:01 +0330

    FormValidation Done

--- FormValidation/script.js
+++ FormValidation/script.js
@@ -7,42 +7,24 @@ document.addEventListener("DOMContentLoaded", () => {
     alerts.innerHTML = `<ul>${messageList}</ul>`;
   };
 
-  const validName = (name) => {
-    name.trim().length >= 3;
-  };
-  console.log('test');
-  console.log(validName(document.getElementById("fname").value));
-
-  const validFamily = (family) => {
-    family.trim().length >= 3;
-  };
-
-  const validAge = (age) => {
-    age >= 18 && age <= 100;
-  };
-
-  const validEmail = (email) => {
-    /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-  };
-
-  const validMobile = (mobile) => {
-    /^09\d{9}$/.test(mobile);
-  };
+  const validName = (fname) => fname.trim().length >= 3;
+  const validFamily = (lname) => lname.trim().length >= 3;
+  const validAge = (age) => age >= 18 && age <= 100;
+  const validEmail = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  const validMobile = (mobile) => /^09\d{9}$/.test(mobile);
 
   myForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const fname = document.getElementById("fname").value.trim();
     const lname = document.getElementById("lname").value.trim();
-    const age = parseInt(document.getElementById("age").value.trim(), 5);
+    const age = parseInt(document.getElementById("age").value.trim());
     const email = document.getElementById("email").value.trim();
     const mobile = document.getElementById("mobile").value.trim();
-
     const errors = [];
 
     if (!validName(fname)) {
       errors.push("نام باید حداقل 3 کاراکتر باشد");
-
     }
 
     if (!validFamily(lname)) {
@@ -64,7 +46,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (errors.length > 0) {
       showAlert(errors);
     } else {
-      showAlert("عملیات با موفقیت انجام شد.");
+      showAlert(["عملیات با موفقیت انجام شد"]);
     }
   });
 });
