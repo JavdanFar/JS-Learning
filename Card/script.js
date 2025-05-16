e0b905e928be75ded886009649828ff5a7d01b0c
Faramarz Javdan <javdanfar@outlook.com>
2025-05-16 11:01:11 +0000

    Cart is Done

--- Card/script.js
+++ Card/script.js
@@ -20,7 +20,7 @@ const updateLocalStorage = () => {
 };
 
 const addToCart = (productId) => {
-  const product = product.find((p) => p.id === productId);
+  const product = products.find((p) => p.id === productId);
   const cartItem = cart.find((item) => item.id === productId);
 
   if (cartItem) {
@@ -30,9 +30,62 @@ const addToCart = (productId) => {
   }
 
   updateLocalStorage();
+  renderCart();
 };
 
-renderCart = () => {}
+const removeFromCart = (productId) => {
+  cart = cart.filter((item) => item.id !== productId);
+  updateLocalStorage();
+  renderCart();
+};
+
+const updateQuantity = (productId, quantity) => {
+  const cartItem = cart.find((item) => item.id === productId);
+
+  if (cartItem) {
+    cartItem.quantity = quantity > 0 ? quantity : 0;
+    if (cartItem.quantity === 0) removeFromCart(productId);
+  }
+  updateLocalStorage();
+  renderCart();
+};
+
+const calcTotal = () => cart.reduce((total, item) => total + item.price * item.quantity, 0);
+
+renderCart = () => {
+  const cartListElement = document.getElementById("cartList");
+  cartListElement.innerHTML = "";
+  cart.forEach((item) => {
+    const li = document.createElement("li");
+    li.className = "list-group-item d-flex flex-column align-items-start mb-3";
+    li.innerHTML = `
+      <div class="d-flex justify-content-between w-100">
+      <strong>${item.name}</strong>
+      <span>${item.price.toLocaleString()} تومان</span>
+      </div>
+      <div class="d-flex justify-content-between w-100 mt-2">
+        <div>
+        <input type="number" class="form-control form-control-sm me-2" style="width: 60px" value="${item.quantity}" min="1"/>
+        <i class="fa-solid fa-trash text-danger me-2" data-id=${item.id} style="cursor:pointer;"></i>
+        </div>
+      </div>
+    `;
+
+    li.querySelector(".fa-trash").addEventListener("click", () => {
+      removeFromCart(item.id);
+    });
+
+    li.querySelector("input").addEventListener("change", (e) => {
+      const quantity = parseInt(e.target.value, 10);
+      updateQuantity(item.id, quantity);
+    });
+
+    cartListElement.appendChild(li);
+  });
+
+  const totalPriceElement = document.getElementById("totalPrice");
+  totalPriceElement.textContent = calcTotal().toLocaleString();
+};
 
 const renderProducts = () => {
   const productListElement = document.getElementById("productList");
@@ -50,9 +103,10 @@ const renderProducts = () => {
     </div>
     </div>
     `;
-    // card.querySelector("button")
+    card.querySelector("button").addEventListener("click", () => addToCart(product.id));
     productListElement.appendChild(card);
   });
 };
 
 renderProducts();
+renderCart();
