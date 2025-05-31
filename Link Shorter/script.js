8aa90af9d2ea66eacf9424afd5f7a93cab5417ef
Faramarz Javdan <javdanfar@outlook.com>
2025-05-31 20:42:14 +0330

    Darkmode Updated

--- Link Shorter/script.js
+++ Link Shorter/script.js
@@ -13,6 +13,13 @@ document.body.dataset.theme = localStorage.getItem("theme") || "light";
 
 const themeBTN = GBI("themeBTN");
 const logo = GBI("logo")
+const theme = localStorage.getItem("theme")
+
+document.body.dataset.theme = theme || "light";
+if (theme == "dark") {
+  themeIcon.classList = ("fa fa-solid fa-sun")
+  logo.src = "./Assets/logo/logo-white.png"
+}
 
 themeBTN.addEventListener("click", () => {
   const body = document.body.dataset;
