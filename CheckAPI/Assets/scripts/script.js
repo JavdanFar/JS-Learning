4df35e089ee1fe1fc85fe31acf4c114c507559ec
Faramarz Javdan <javdanfar@outlook.com>
2025-05-31 23:28:29 +0330

    Tabs Added

--- CheckAPI/Assets/scripts/script.js
+++ CheckAPI/Assets/scripts/script.js
@@ -28,3 +28,34 @@ themeBTN.addEventListener("click", () => {
         logo.src = "./Assets/logo/logo-white.png"
     }
 });
+
+
+// =======***======= Tabs Section =======***=======
+
+const paramsBTN = GBI("paramsBTN")
+const headersBTN = GBI("headersBTN")
+const bodyBTN = GBI("bodyBTN")
+
+const paramsTab = GBI("paramsTab")
+const headersTab = GBI("headersTab")
+const bodyTab = GBI("bodyTab")
+
+const handleTab = function (tab, activeBTN) {
+    paramsTab.style.display = "none"
+    headersTab.style.display = "none"
+    bodyTab.style.display = "none"
+
+    tab.style.display = "flex"
+
+    paramsBTN.classList.remove("active-btn");
+    headersBTN.classList.remove("active-btn");
+    bodyBTN.classList.remove("active-btn");
+
+    activeBTN.classList.add("active-btn");
+}
+
+paramsBTN.addEventListener("click", () => { handleTab(paramsTab, paramsBTN) })
+headersBTN.addEventListener("click", () => { handleTab(headersTab, headersBTN) })
+bodyBTN.addEventListener("click", () => { handleTab(bodyTab, bodyBTN) })
+handleTab(paramsTab, paramsBTN)
+
